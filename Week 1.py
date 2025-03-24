b7e01fae767f667a6baaa332c9f07311602547bb
Coconut-cee <otafucovenant@gmail.com>
2025-03-24 18:15:57 +0100

    Create Week 1.py

---
+++ Week 1.py
@@ -0,0 +1,18 @@
+group_data = {
+    "Covenant" : {"Name": "Covenant Otafu", "Hobbies":['Reading', 'crocheting', 'learning new things'], "Country": "Nigeria", "Discipline": "Pharmacy", "Language" : "Python" },
+    "Victor" : {"Name": "Victor Chinecherem", "Hobies": ['Listening to old school music', 'watching scary movies', 'listening to audio books'], "Country": "Nigeria", "Discipline": "Ecotoxicology and Conservation", "Language": "Python" },
+    "Jesuranti": {"Name": "Jesuranti Samuel", "Hobbies": ['Football', 'Learning new things', 'Reading'], "Country": "Nigeria", "Discipline": "Computational Biology", "Language": "Python"},
+    "Mariam": {"Name": "Quadri Mariam Folake", "Hobbies": ['Reading', 'writing', 'sleeping', 'watching movies', 'learning new things'], "Country": "Nigeria", "Discipline": "Undecided", "Language": "R"   }
+}
+
+print(group_data["Covenant"])
+print(group_data["Victor"])
+print(group_data["Jesuranti"])
+print(group_data["Mariam"])
+
+
+
+
+
+
+
